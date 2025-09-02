af1706b70163ae3b95f0bb49048ef6ecd5a62868
LoyisoMotsamai <128675393+LoyisoMotsamai@users.noreply.github.com>
2025-09-02 21:48:07 +0200

    big push, intial things here

---
+++ Loyiso_Municipality/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Loyiso_Municipality.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
